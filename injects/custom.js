a37a3f2a0937a8fa9fd9fa3d153d2c95736c4c30
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-02-29 13:32:18 -0600

    Update custom.js

--- injects/custom.js
+++ injects/custom.js
@@ -7,8 +7,8 @@ void async function LinkResolver(){
         swapText('wall street journal','Financial Journal');
         swapText('wallstreet journal','Financial Journal');
         swapText('wsj','Financial Journal');
-        updateProperty(document,'title', document.title.replace(/wall street journal/gi,'Financial Journal');
-        updateProperty(document,'title', document.title.replace(/wallstreet journal/gi,'Financial Journal');
-        pdateProperty(document,'title', document.title.replace(/wsj/gi,'Financial Journal');
+        updateProperty(document,'title', document.title.replace(/wall street journal/gi,'Financial Journal'));
+        updateProperty(document,'title', document.title.replace(/wallstreet journal/gi,'Financial Journal'));
+        updateProperty(document,'title', document.title.replace(/wsj/gi,'Financial Journal'));
       });
 }();
