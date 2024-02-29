4582b3349f5a1568b84c242076ffa52de3e5fded
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-02-29 12:57:03 -0600

    Create link-resolver.js

---
+++ injects/link-resolver.js
@@ -0,0 +1,32 @@
+
+void async function LinkResolver(){
+  if(!globalThis.declare){
+    await import('https://unpkg.com/javaxscript/framework.js');
+  }
+  globalThis.hostList = ['www.wsj.com'];
+      declare(() => {
+          const hostList = globalThis.hostList;
+          const hostList_length = hostList.length;
+
+          const attrs = ['src', 'href'];
+          const attrs_length = attrs.length;
+
+          for (let i = 0; i < hostList_length; i++) {
+              const host = hostList[i];
+              for (let x = 0; x < attrs_length; x++) {
+                  const attr = attrs[x];
+                  queryApplyAll(`[${attr}*="https://${host}"]`, (el) => {
+                      try {
+                          let c = '?';
+                          if (el[attr].includes('?')) {
+                              c = '&';
+                          }
+                          el.updateAttribute(attr, el[attr].replaceAll(host, location.host) + c + 'hostname=' + host);
+                      } catch (e) {
+                          console.log(e.message);
+                      }
+                  });
+              }
+          }
+      });
+}();
