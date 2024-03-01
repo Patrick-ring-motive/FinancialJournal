54149897c5a499107d9cf28ec578083b0948c0b2
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-02-29 21:14:16 -0600

    Update fetch-redirect.js

--- injects/fetch-redirect.js
+++ injects/fetch-redirect.js
@@ -1,14 +1,20 @@
+globalThis.fetchRedirects=[
+  ['https://api.wsj.net','https://api-financial.patrickring.net'],
+  ['https://www.wsj.com','https://financial.patrickring.net']
+];
 if(!globalThis.nativeFetch){
   globalThis.nativeFetch=globalThis.fetch;
   globalThis.fetch=async function(){
-    let args = Array.from(arguments)
-    if(typeof args[0] == 'string'){
-      if(args[0].startsWith('https://api.wsj.net')){
-        args[0]=args[0].replace('https://api.wsj.net','https://api-financial.patrickring.net');
-      }
-    }else{
-      if(`${args[0].url}`.startsWith('https://api.wsj.net')){
-        args[0]=new Request(`${args[0].url}`.replace('https://api.wsj.net','https://api-financial.patrickring.net'),args[0]);
+    for(let i=0;i<fetchRedirects.length;i++){
+      let args = Array.from(arguments)
+      if(typeof args[0] == 'string'){
+        if(args[0].startsWith(fetchRedirects[i][0])){
+          args[0]=args[0].replace(fetchRedirects[i][0],fetchRedirects[i][1]);
+        }
+      }else{
+        if(`${args[0].url}`.startsWith(fetchRedirects[i][0])){
+          args[0]=new Request(`${args[0].url}`.replace(fetchRedirects[i][0],fetchRedirects[i][1]),args[0]);
+        }
       }
     }
     return globalThis.nativeFetch(...args);
