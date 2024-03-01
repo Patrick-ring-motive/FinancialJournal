bf838c57cc26933303e2ab7033912d0932150d5c
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-02-29 21:07:16 -0600

    Update fetch-redirect.js

--- injects/fetch-redirect.js
+++ injects/fetch-redirect.js
@@ -3,12 +3,12 @@ if(!globalThis.nativeFetch){
   globalThis.fetch=async function(){
     let args = Array.from(arguments)
     if(typeof args[0] == 'string'){
-      if(args[0].startsWith('https://api.wsj.com')){
-        args[0]=args[0].replace('https://api.wsj.com','https://api-financial.patrickring.net');
+      if(args[0].startsWith('https://api.wsj.net')){
+        args[0]=args[0].replace('https://api.wsj.net','https://api-financial.patrickring.net');
       }
     }else{
-      if(`${args[0].url}`.startsWith('https://api.wsj.com')){
-        args[0]=new Request(`${args[0].url}`.replace('https://api.wsj.com','https://api-financial.patrickring.net'),args[0]);
+      if(`${args[0].url}`.startsWith('https://api.wsj.net')){
+        args[0]=new Request(`${args[0].url}`.replace('https://api.wsj.net','https://api-financial.patrickring.net'),args[0]);
       }
     }
     return globalThis.nativeFetch(...args);
